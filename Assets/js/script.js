d0eac31d89edb209eb18f7ab54a81e31a3c6ca4b
pdhende <pdhendework@gmail.com>
2022-01-22 00:56:21 -0500

    Updated script.js to include function for setting and resetting timer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,21 @@
 // Fetch the elements
 var startQuiz = document.querySelector("#start");
 var timeValue = document.querySelector(".timer-value");
+var intervalID;
 
-var setTimer = function() {
+// Function to set the timer
+function setTimer() {
     var timerCount = 75;
-    timeValue.textContent = timerCount;
+    function setTime() {
+        timerCount--;
+        timeValue.textContent = timerCount;
+
+        if(timerCount == 0) {
+            clearInterval(intervalID);
+        }
+    }
+    intervalID = setInterval(setTime, 1000);
 }
 
+// Event listeners to capture click of a button
 startQuiz.addEventListener("click", setTimer);
